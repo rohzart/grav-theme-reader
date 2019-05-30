b70d0f00b9cea4658f8254a39e35475a35d0cf30
rohzart <rohzart@gmail.com>
2019-05-30 19:14:50 +0530

    typography

--- tscript/common.ts
+++ tscript/common.ts
@@ -2,14 +2,15 @@ import * as feather from "./../node_modules/feather-icons/dist/feather.js";
 
 $(document).ready(function(){
     feather.replace();
+    // maybe add blurblur to ", header" later?
     $('#openmenu').click(function(){
         $('#overlay, #menu').removeClass('close');
         $('#overlay, #menu').addClass('open');
-        $('#content, header, footer').addClass('blurblur');
+        $('#content, footer').addClass('blurblur');
     });
     $('#overlay, #closemenu').click(function(){
         $('#overlay, #menu').removeClass('open');
         $('#overlay, #menu').addClass('close');
-        $('#content, header, footer').removeClass('blurblur');
+        $('#content, footer').removeClass('blurblur');
     });
 });
